5a52093d1eac10c2f8d2ecb0ff3044825e4b190f
黄家强 <623536509@qq.com>
2023-06-12 09:31:35 +0800

    feat: 数据大屏

--- src/views/Screen/components/Age/index.vue
+++ src/views/Screen/components/Age/index.vue
@@ -1,13 +1,73 @@
 <template>
-  <div class="age_box">age</div>
+  <div class="age_box">
+    <div class="top_title">
+      <p>年龄比例</p>
+      <img src="../../images/dataScreen-title.png" alt="" />
+    </div>
+    <div class="my_echarts" ref="charts"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+const charts = ref()
+onMounted(() => {
+  const myEcharts = echarts.init(charts.value)
+  myEcharts.setOption({
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: '15%',
+      left: 'right', //一整个容器对其方式
+      align: 'left', //文字对齐方式
+      orient: 'vertical', //垂直排列
+      itemGap: 20,
+      textStyle: {
+        color: 'white',
+      },
+    },
+    series: [
+      {
+        name: '本周预约量',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        itemStyle: {
+          borderRadius: 10,
+        },
+        label: {
+          show: true,
+          formatter: '{a}',
+          position: 'center',
+          color: '#eee',
+        },
+        data: [
+          { value: 20, name: '10岁以下' },
+          { value: 10, name: '10-18岁' },
+          { value: 30, name: '18-30岁' },
+          { value: 10, name: '30-40岁' },
+          { value: 15, name: '40-60岁' },
+          { value: 15, name: '60岁以上' },
+        ],
+      },
+    ],
+  })
+})
+</script>
 
-<style scoped>
+<style scoped lang="scss">
 .age_box {
   background: url(../../images/dataScreen-main-lc.png) no-repeat 100% 100% /
     cover;
-  margin: 0 8px;
+  margin: 0 8px 15px 8px;
+  .top_title {
+    font-size: 20px;
+    color: white;
+  }
+  .my_echarts {
+    width: 100%;
+    height: 85%;
+  }
 }
 </style>
--- src/views/Screen/components/Sex/index.vue
+++ src/views/Screen/components/Sex/index.vue
@@ -6,16 +6,82 @@
     </div>
     <div class="sex_img">
       <div class="man">
+        <span>男士</span>
         <img src="../../images/man.png" alt="" />
       </div>
       <div class="woman">
+        <span>女士</span>
         <img src="../../images/woman.png" alt="" />
       </div>
     </div>
+    <div class="my_echarts" ref="charts"></div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as echarts from 'echarts'
+import { onMounted, ref } from 'vue'
+const charts = ref()
+onMounted(() => {
+  //初始化echarts
+  const myEcharts = echarts.init(charts.value)
+  myEcharts.setOption({
+    xAxis: {
+      show: false,
+      min: 0,
+      max: 100,
+    },
+    yAxis: {
+      show: false,
+      type: 'category', //让柱子横向
+    },
+    series: [
+      {
+        type: 'bar',
+        data: [58], //柱子的数据
+        barWidth: 20, //设置柱子的宽度
+        z: 3, //设置柱子的层级，默认是2，设高一点不被下面的柱子覆盖
+        name: '男士',
+        label: {
+          show: true, //设置是否显示文字
+          formatter: '{a}{c}%', //设置文字，官网可查
+          fontSize: 20,
+          color: 'white',
+          position: [0, -20], //设置文字的位置
+        },
+        itemStyle: {
+          color: '#0075ff',
+          borderRadius: 20, //设置柱条圆角
+        },
+      },
+      {
+        type: 'bar',
+        data: [100], //柱子的数据
+        barWidth: 20, //设置柱子的宽度
+        barGap: '-100%', //将两个柱子重叠
+        name: '女士',
+        label: {
+          show: true, //设置是否显示文字
+          formatter: '{a}42%', //设置文字，官网可查
+          fontSize: 20,
+          position: ['75%', -20], //设置文字的位置
+          color: 'white',
+        },
+        itemStyle: {
+          color: '#ff5f79',
+          borderRadius: 20, //设置柱条圆角
+        },
+      },
+    ],
+    // grid: {
+    //   left: 0,
+    //   top: 0,
+    //   right: 0,
+    //   bottom: 0,
+    // },
+  })
+})
+</script>
 
 <style scoped lang="scss">
 .sex_box {
@@ -30,8 +96,36 @@
     display: flex;
     justify-content: center;
     div {
-      margin: 30px;
+      margin: 30px 40px;
     }
+    .man {
+      background: url(../../images/man-bg.png) no-repeat 100% 100% / cover;
+    }
+    .woman {
+      background: url(../../images/woman-bg.png) no-repeat 100% 100% / cover;
+    }
+    .man,
+    .woman {
+      padding: 30px 20px 15px 20px;
+      position: relative;
+      img {
+        width: 60px;
+        height: 62px;
+      }
+      span {
+        position: absolute;
+        color: white;
+        font-size: 18px;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+  }
+  .my_echarts {
+    margin: 0 auto;
+    width: 88%;
+    height: 100px;
   }
 }
 </style>
